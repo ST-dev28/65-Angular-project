71ed733887a66db72a30c50237336a65f0260441
SandraT <sndrbltr@gmail.com>
2021-10-20 10:24:57 +0000

    Direktyvos

---
+++ src/app/models/Prezidentas.ts
@@ -0,0 +1,22 @@
+// "models" folderyje saugosime atskiras klases
+//modelis yra atsakingas uz savo duomenu saugojima
+export class Prezidentas {
+    public vardas: string;
+    private _reitingas: number = 0;
+
+    public constructor(vardas: string) {
+        this.vardas = vardas;
+    }
+
+    public pridetiReitingo(): void {
+        this._reitingas++;
+    }
+
+    public atimtiReitingo(): void {
+        this._reitingas--;
+    }
+
+    public get reitingas(): number {
+        return this._reitingas;
+    }
+}
